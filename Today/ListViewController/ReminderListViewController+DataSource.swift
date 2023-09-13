69272bd0ff8f8414787d884e70ed1ab278457517
Marcus Gugacs <iimpaq@proton.me>
2023-09-13 20:35:36 +0200

    Fixed adding reminders

--- Today/ListViewController/ReminderListViewController+DataSource.swift
+++ Today/ListViewController/ReminderListViewController+DataSource.swift
@@ -22,7 +22,7 @@ extension ReminderListViewController {
     func updateSnapshot(reloading ids: [Reminder.ID] = []) {
         var snapshot = Snapshot()
         snapshot.appendSections([0])
-        snapshot.appendItems(Reminder.sampleData.map{ $0.id })
+        snapshot.appendItems(reminders.map{ $0.id })
         if !ids.isEmpty {
             snapshot.reloadItems(ids)
         }
