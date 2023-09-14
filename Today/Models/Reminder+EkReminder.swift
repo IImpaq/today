7e72c9b163b7b6c98cd3673821e499de849b3789
Marcus Gugacs <iimpaq@proton.me>
2023-09-14 11:35:27 +0200

    Functionality to load reminders

---
+++ Today/Models/Reminder+EkReminder.swift
@@ -0,0 +1,22 @@
+//
+//  Reminder+EkReminder.swift
+//  Today
+//
+//  Created by Marcus Gugacs on 14.09.23.
+//
+
+import Foundation
+import EventKit
+
+extension Reminder {
+    init(with ekReminder: EKReminder) throws {
+        guard let dueDate = ekReminder.alarms?.first?.absoluteDate else {
+            throw TodayError.reminderHasNoDueDate
+        }
+        id = ekReminder.calendarItemIdentifier
+        title = ekReminder.title
+        self.dueDate = dueDate
+        notes = ekReminder.notes
+        isComplete = ekReminder.isCompleted
+    }
+}
